f21ad441dee8a5731fe202d4510237e288e302d7
Ricardo R. Sierra <sierra.csi@gmail.com>
2022-07-16 11:21:27 -0300

    games

--- database/migrations/2020_09_18_231619_create_gamer_guest_tables.php
+++ database/migrations/2020_09_18_231619_create_gamer_guest_tables.php
@@ -22,6 +22,31 @@ class CreateGamerGuestTables extends Migration
                 $table->softDeletes();
             }
         );
+        Schema::create(
+            'questions', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->increments('id')->unsigned();
+                $table->string('event', 255)->nullable();
+                $table->integer('user_id')->nullable();
+                $table->integer('position')->nullable();
+                $table->integer('value')->nullable();
+                $table->timestamps();
+                $table->softDeletes();
+            }
+        );
+        Schema::create(
+            'question_responses', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->increments('id')->unsigned();
+                $table->integer('position')->nullable();
+                $table->integer('value')->nullable();
+
+                $table->integer('user_id')->nullable();
+                $table->integer('question_id')->nullable();
+                $table->timestamps();
+                $table->softDeletes();
+            }
+        );
     }
 
     /**
